ed420ab71023bde44cd085e1bd3bc1a7014ad353
Dylan Martie <dylan.martie@ndus.edu>
2019-12-09 16:53:46 -0600

    Added Login Functions

---
+++ HospitalOnlineSystemGroup12/HospitalOnlineSystemGroup12/Login.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="HospitalOnlineSystemGroup12.Login" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p>
+    <br />
+</p>
+<p>
+    <asp:Login ID="Login1" runat="server" Height="381px" OnAuthenticate="Login1_Authenticate" Width="599px" BackColor="#E3EAEB" BorderColor="#E6E2D8" BorderPadding="4" BorderStyle="Solid" BorderWidth="1px" Font-Names="Verdana" Font-Size="Large" ForeColor="#333333">
+        <InstructionTextStyle Font-Italic="True" ForeColor="Black" />
+        <LoginButtonStyle BackColor="White" BorderColor="#C5BBAF" BorderStyle="Solid" BorderWidth="1px" Font-Names="Verdana" Font-Size="0.8em" ForeColor="#1C5E55" />
+        <TextBoxStyle Font-Size="0.8em" />
+        <TitleTextStyle BackColor="#1C5E55" Font-Bold="True" Font-Size="0.9em" ForeColor="White" />
+    </asp:Login>
+</p>
+<p>
+</p>
+<p>
+</p>
+<p>
+</p>
+<p>
+</p>
+</asp:Content>
---
+++ HospitalOnlineSystemGroup12/HospitalOnlineSystemGroup12/Login.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+
+namespace HospitalOnlineSystemGroup12
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        HospitalDBEntities dbcon = new HospitalDBEntities();
+       
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UnobtrusiveValidationMode = System.Web.UI.UnobtrusiveValidationMode.None;
+        }
+
+        protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+            List<UsersTable> users = dbcon.UsersTables.ToList();    // Creates list of all users
+
+            // Verfies Login information
+            foreach (UsersTable user in users)
+            {
+                if (user.UserLoginName.Trim().Equals(Login1.UserName))
+                {
+                    if (user.UserLoginPass.Trim().Equals(Login1.Password))
+                    {
+                        // Transfers needed UserNameLogin and IsDoctor properties to Homepage
+                        Context.Items.Add("LoginName", user.UserLoginName);
+                        Context.Items.Add("IsDoctor", user.UserIsDoctor);
+                        Server.Transfer("HomePage.aspx", true);
+                    }
+                        
+                }
+            }
+        }
+    }
+}

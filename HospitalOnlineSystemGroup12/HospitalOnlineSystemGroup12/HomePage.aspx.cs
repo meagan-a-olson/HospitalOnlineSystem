fc589cce2c0e61dfde7163a609bd8e5000a06eb0
Dylan Martie <dylan.martie@ndus.edu>
2019-12-11 15:38:06 -0600

    Changed Context to Session

--- HospitalOnlineSystemGroup12/HospitalOnlineSystemGroup12/HomePage.aspx.cs
+++ HospitalOnlineSystemGroup12/HospitalOnlineSystemGroup12/HomePage.aspx.cs
@@ -15,13 +15,13 @@ namespace HospitalOnlineSystemGroup12
         protected void Page_Load(object sender, EventArgs e)
         {
             // Checks if user is patient or doctor, and creates object
-            if (Convert.ToInt32(Context.Items["IsDoctor"]) == 0)
+            if (Convert.ToInt32(Session["IsDoctor"]) == 0)
             {
                 List<PatientsTable> users = dbcon.PatientsTables.ToList();
                 foreach (PatientsTable patient in users)
                 {
                     // Info transferred in from login in Context
-                    if (Context.Items["LoginName"].ToString().Equals(patient.UserLoginName))
+                    if (Session["LoginName"].ToString().Equals(patient.UserLoginName))
                     {
                         myPatient.PatientID = patient.PatientID;
                         myPatient.DoctorID = patient.DoctorID;
@@ -42,7 +42,7 @@ namespace HospitalOnlineSystemGroup12
                 foreach (DoctorsTable doctor in users)
                 {
                     // Info transferred in from login in Context
-                    if (Context.Items["LoginName"].ToString().Equals(doctor.UserLoginName))
+                    if (Session["LoginName"].ToString().Equals(doctor.UserLoginName))
                     {
                         myDoctor.DoctorID = doctor.DoctorID;
                         myDoctor.FirstName = doctor.FirstName;
@@ -57,7 +57,7 @@ namespace HospitalOnlineSystemGroup12
 
 
             // Page load depends on whether Patient or Doctor
-            if (Convert.ToInt32(Context.Items["IsDoctor"]) == 0)
+            if (Convert.ToInt32(Session["IsDoctor"]) == 0)
             {
                 Label1.Text = myPatient.FirstName + " " + myPatient.LastName;
                 Label2.Text = "Your Doctor is: ";
